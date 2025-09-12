10514bfbdd9f5774ed6a11ccc5766f3646d8fc33
DemiusLs <dloss320@gmail.com>
2025-09-12 12:04:02 +0200

    feat: cuffie class done

---
+++ src/org/lesson/java/shop/Cuffie.java
@@ -0,0 +1,44 @@
+package org.lesson.java.shop;
+
+import java.math.BigDecimal;
+
+public class Cuffie extends Prodotto{
+
+
+    //attributi
+    private String colore;
+    private boolean isWireless;
+
+    //costruttore
+    public Cuffie(){
+        
+        this.colore = "nessun colore";
+        this.isWireless = false;
+    }
+
+    public Cuffie(String nome , String marca , BigDecimal prezzo , BigDecimal iva ,String colore, boolean isWireless){
+
+        super(  nome , marca ,  prezzo , iva);
+        this.colore = colore ;
+        this.isWireless = isWireless;
+        
+    }
+
+    //metodi
+
+    public String getColore(){
+        return this.colore;
+    }
+    public void setColore(String colore){
+        this.colore = colore;
+    }
+
+ 
+    public boolean getIsWireless(){
+        return this.isWireless;
+    }
+
+    public void setIsWireless(boolean isWireless){
+        this.isWireless = isWireless;
+    }
+}
